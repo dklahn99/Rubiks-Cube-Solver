213daf6aec62d66c77445fea034f27900f097d90
Daniel Klahn <dklahn@mit.edu>
2019-12-20 17:40:08 -0700

    Wrote specifications for Side methods

--- src/solver/Side.java
+++ src/solver/Side.java
@@ -13,7 +13,8 @@ public class Side {
         Abstraction function:
 
             AF(colors) = a 3x3 grid of colors where colors.get(x).get(y) is the color at coordinate (x,y)
-                         and (0, 0) is the lower left corner of the grid.
+                         and (0, 0) is the lower left corner of the grid. x corresponds to the column and
+                         y corresponds to the row.
     
         Rep invariant:
           - colors.size() == 3
@@ -24,26 +25,27 @@ public class Side {
      */
     
     /**
-     * 
-     * @param colors
+     * Creates an immutable Side object where where colors.get(x).get(y) is the color at coordinate (x,y)
+     * and (0, 0) is the lower left corner of the grid. x corresponds to the column and
+     * y corresponds to the row.
+     * @param colors the 3x3 list of lists of colors
      */
     Side(List<List<Color>> colors) {
         throw new RuntimeException("not yet implemented");
     }
     
     /**
-     * 
-     * @param x
-     * @param y
-     * @return
+     * @param x the column
+     * @param y the row
+     * @return the color at (x,y) on the side
      */
     Color get(int x, int y) {
         throw new RuntimeException("not yet implemented");
     }
     
     /**
-     * 
-     * @return
+     * @return a 3x3 list of lists of colors, output, where output.get(x).get(y) is the color at coordinate (x,y)
+               and (0, 0) is the lower left corner of the side.
      */
     List<List<Color>> grid() {
         throw new RuntimeException("not yet implemented");
